3a1b1b07913b5930f0906581e0dedf353dccaa08
Gilles CRUCHON <gilles.cruchon@axa.com>
2022-02-06 22:09:28 +0100

    feat: get distance and duration from event list

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
+import { readFileSync } from "fs";
 import getDistances from "./getDistances.js"
 
-getDistances("59110", ["31000", "33000", "75015", "06000"])
-    .then(function (response) {
+const dataFile = "data/events-AF-2022.json";
+const myPostalCode = "59110"
+
+const events = JSON.parse(readFileSync(dataFile));
+const destinations = events.map(e => e.location.postalCode);
+
+getDistances(myPostalCode, destinations)
+    .then((distances) => {
         // Add your own result handling here
-        console.log("response", response);
+        const eventWithDistance = events.map(e => {
+            const matrix = distances.destinations[e.location.postalCode];
+            return {...e, ...matrix};
+        });
+        console.log("eventWithDistance", eventWithDistance);
     })
-    .catch(function (err) {
+    .catch((err) => {
         var str = "An error occurred: " + err;
         console.log(str);
     });
